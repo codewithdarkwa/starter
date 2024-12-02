c624584713a09e2092a86af0e4d2cb327dc1b7a4
Darkwa John <jnrdrkw1@gmail.com>
2024-12-02 11:55:21 +0000

    update

--- src/main.js
+++ src/main.js
@@ -1,17 +1,18 @@
 import { Client, Databases, Users, Messaging } from "node-appwrite";
 
-const PROJECT_ID = process.env.PROJECT_ID
+const PROJECT_ID = process.env.PROJECT_ID;
 const DATABASE_ID = process.env.DATABASE_ID;
 const COLLECTION_ID_TASKS = process.env.COLLECTION_ID_TASKS;
 
+const GENERAL_UPDATE_ID = process.env.GENERAL_TOPIC_ID;
+const TECHNICAL_SUPPORT_ID = process.env.TECHNICAL_SUPPORT_ID;
+const PRODUCT_ANNOUNCEMENT_ID = process.env.PRODUCT_ANNOUNCEMENT_ID;
 
 export default async({req, res, log, error}) => {
-
   const client = new Client()
-  .setEndpoint('https://cloud.appwrite.io/v1')
-  .setProject(PROJECT_ID)
-  .setKey(process.env.APPWRITE_SERVER_API_KEY);
-
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject(PROJECT_ID)
+    .setKey(process.env.APPWRITE_SERVER_API_KEY);
 
   const users = new Users(client);
   const db = new Databases(client);
@@ -29,22 +30,40 @@ export default async({req, res, log, error}) => {
         }, 400);
       }
 
-      try {
-        const subscriptions = await messaging.listSubscribers('6745c75f00118025ce9a');
-        log(subscriptions);
-        const userSubscriptions = subscriptions.subscribers.filter(sub => sub.targetId === userId);
+      const topicIds = [
+        GENERAL_UPDATE_ID, 
+        TECHNICAL_SUPPORT_ID, 
+        PRODUCT_ANNOUNCEMENT_ID
+      ];
 
-        for (const subscription of userSubscriptions) {
-          await messaging.deleteSubscriber(subscription.$id);
+      let totalUnsubscribed = 0;
+
+      try {
+        for (const topicId of topicIds) {
+          try {
+            const subscriptions = await messaging.listSubscribers(topicId);
+            const userSubscriptions = subscriptions.subscribers.filter(sub => sub.targetId === userId);
+
+            for (const subscription of userSubscriptions) {
+              await messaging.deleteSubscriber(subscription.$id);
+              totalUnsubscribed++;
+            }
+          } catch (topicError) {
+            error(`Failed to unsubscribe from topic ${topicId}: ${topicError.message}`);
+          }
         }
 
-        log(`Unsubscribed user ${userId} from ${userSubscriptions.length} topics`);
-      } catch (topicError) {
-        error("Failed to unsubscribe from topics: " + topicError.message);
+        log(`Unsubscribed user ${userId} from ${totalUnsubscribed} topic subscriptions`);
+      } catch (unsubscribeError) {
+        error("Error during unsubscription process: " + unsubscribeError.message);
       }
 
       await users.deleteSessions(userId, sessionId);
-      return res.json({ success: true, message: 'User logged out successfully' });
+
+      return res.json({ 
+        success: true, 
+        message: `User logged out and unsubscribed from ${totalUnsubscribed} topics successfully` 
+      });
     } catch (err) {
       error("Logout failed: " + err.message);
       return res.json({ success: false, message: 'Logout failed' });
@@ -55,44 +74,4 @@ export default async({req, res, log, error}) => {
     const response = await db.listDocuments(DATABASE_ID, COLLECTION_ID_TASKS);
     return res.json(response.documents);
   }
-
-
-
-  
 }
-
-
-
-// // This Appwrite function will be executed every time your function is triggered
-// export default async ({ req, res, log, error }) => {
-//   // You can use the Appwrite SDK to interact with other services
-//   // For this example, we're using the Users service
-//   const client = new Client()
-//     .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
-//     .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-//     .setKey(req.headers['x-appwrite-key'] ?? '');
-//   const users = new Users(client);
-
-//   try {
-//     const response = await users.list();
-//     // Log messages and errors to the Appwrite Console
-//     // These logs won't be seen by your end users
-//     log(`Total users: ${response.total}`);
-//   } catch(err) {
-//     error("Could not list users: " + err.message);
-//   }
-
-//   // The req object contains the request data
-//   if (req.path === "/ping") {
-//     // Use res object to respond with text(), json(), or binary()
-//     // Don't forget to return a response!
-//     return res.text("Pong");
-//   }
-
-//   return res.json({
-//     motto: "Build like a team of hundreds_",
-//     learn: "https://appwrite.io/docs",
-//     connect: "https://appwrite.io/discord",
-//     getInspired: "https://builtwith.appwrite.io",
-//   });
-// };
